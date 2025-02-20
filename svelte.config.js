e94c29842eca57322cafd66c120017f352ee9610
Fram <fram.tid.fash@gmail.com>
2025-02-20 11:37:52 -0600

    errors

--- svelte.config.js
+++ svelte.config.js
@@ -8,20 +8,28 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+		prerender: {
+			handleHttpError: ({path, referrer, message}) => {
+				if (path === '/not-found' && referrer === '/blog/how-we-built-our-404-page') {
+					return;
+				}
+			}
+		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: null,
 			precompress: false,
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.env.NODE_ENV === 'production' ? '/cdmx_prop' : '',
 		}
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
